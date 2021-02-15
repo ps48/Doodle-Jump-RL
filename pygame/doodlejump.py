5be855f76ab53c91156f234772caf03713c680d4
Shenoy Pratik Gurudatt <gurudatt@usc.edu>
2021-02-15 12:29:55 -0800

    added game-agent interaction functions

--- pygame/doodlejump.py
+++ pygame/doodlejump.py
@@ -4,7 +4,7 @@ import sys
 import random
 
 class DoodleJump:
-    def __init__(self):
+    def __init__(self, FPS=30000):
         self.screen = pygame.display.set_mode((800, 800))
         self.green = pygame.image.load("assets/green.png").convert_alpha()
         pygame.font.init()
@@ -32,11 +32,14 @@ class DoodleJump:
         self.xmovement = 0
         self.die= 0
         self.inter_platform_distance = 65
-    
+        self.FPSCLOCK = clock = pygame.time.Clock()
+        self.FPS = FPS
+        self.timer = None
+
     def updatePlayer(self):
         if self.die==1:
             return
-        if not self.jump:        
+        if not self.jump:
             self.playery += self.gravity
             self.gravity += 1
         elif self.jump:
@@ -79,7 +82,7 @@ class DoodleJump:
         for p in self.platforms:
             rect = pygame.Rect(p[0], p[1], self.green.get_width() - 10, self.green.get_height())
             player = pygame.Rect(self.playerx, self.playery, self.playerRight.get_width() - 10, self.playerRight.get_height())
-            
+
             if rect.colliderect(player) and self.gravity and self.playery < (p[1] - self.cameray):
                 if p[2] != 2:
                     self.jump = 15
@@ -90,8 +93,8 @@ class DoodleJump:
                         p[-1] = 1
                     else:
                         self.jump = 0
-                    
-            
+
+
             # moving blue platform left and right
             if p[2] == 1:
                 if p[-1] == 1:
@@ -104,6 +107,7 @@ class DoodleJump:
                         p[-1] = 1
 
     def drawPlatforms(self):
+        score_increment = False
         for p in self.platforms:
             #print((self.platforms))
             check = self.platforms[1][1] - self.cameray
@@ -121,13 +125,14 @@ class DoodleJump:
                 check = random.randint(0, 1000)
                 if check > 900 and platform == 0:
                     self.springs.append([coords[0], coords[1] - 25, 0])
-                
+
                 elif check>860 and platform == 0:
                     self.monsters.append([coords[0], coords[1]- 50, 0])
-                
+
                 print(self.platforms.pop(0))
                 self.score += 100
-            
+                score_increment = True
+
             if p[2] == 0:
                 self.screen.blit(self.green, (p[0], p[1] - self.cameray))
             elif p[2] == 1:
@@ -137,7 +142,7 @@ class DoodleJump:
                     self.screen.blit(self.red, (p[0], p[1] - self.cameray))
                 else:
                     self.screen.blit(self.red_1, (p[0], p[1] - self.cameray))
-    
+
         for spring in self.springs:
             if spring[-1]:
                 self.screen.blit(self.spring_1, (spring[0], spring[1] - self.cameray))
@@ -150,7 +155,9 @@ class DoodleJump:
             self.screen.blit(self.monster, (monster[0], monster[1] -self.cameray))
             if pygame.Rect(monster[0], monster[1], self.monster.get_width(), self.monster.get_height()).colliderect(pygame.Rect(self.playerx, self.playery, self.playerRight.get_width(), self.playerRight.get_height())):
                 self.die=1
-    
+
+        return score_increment
+
     def generatePlatforms(self):
         on = 800
         while on > -100:
@@ -169,7 +176,147 @@ class DoodleJump:
         for x in range(80):
             pygame.draw.line(self.screen, (222,222,222), (x * 12, 0), (x * 12, 800))
             pygame.draw.line(self.screen, (222,222,222), (0, x * 12), (800, x * 12))
-    
+
+    def getCurrentFrame(self):
+        """
+            - No Param function
+            - Returns
+                - A screengrab of current pygame display
+            - To be used by agent script to get current state
+        """
+        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        return image_data
+
+    def updatePlayerByAction(self, actions):
+        """
+            - actions = ['ACTION_LEFT', 'NO_ACTION', 'ACTION_RIGHT']
+            - Param:
+                - actions: a list that contains three boolean value.
+            - To be used by playStep function in game script.
+
+        """
+        if not self.jump:
+            self.playery += self.gravity
+            self.gravity += 1
+        elif self.jump:
+            self.playery -= self.jump
+            self.jump -= 1
+
+        if actions[2]:
+            if self.xmovement < 10:
+                self.xmovement += 1
+            self.direction = 0
+
+        elif actions[0]:
+            if self.xmovement > -10:
+                self.xmovement -= 1
+            self.direction = 1
+
+        else: #action[1] is true
+            if self.xmovement > 0:
+                self.xmovement -= 1
+            elif self.xmovement < 0:
+                self.xmovement += 1
+
+        if self.playerx > 850:
+            self.playerx = -50
+        elif self.playerx < -50:
+            self.playerx = 850
+
+        # if self.playerx > 750:
+        #     #self.playerx = -50
+        #     self.playerx = 750
+        # elif self.playerx < 0:
+        #     #self.playerx = 850
+        #     self.playerx = 0
+
+        self.playerx += self.xmovement
+        if self.playery - self.cameray <= 200:
+            self.cameray -= 10
+        if not self.direction:
+            if self.jump:
+                self.screen.blit(self.playerRight_1, (self.playerx, self.playery - self.cameray))
+            else:
+                self.screen.blit(self.playerRight, (self.playerx, self.playery - self.cameray))
+        else:
+            if self.jump:
+                self.screen.blit(self.playerLeft_1, (self.playerx, self.playery - self.cameray))
+            else:
+                self.screen.blit(self.playerLeft, (self.playerx, self.playery - self.cameray))
+
+    def playStep(self, actions):
+        """
+            - actions = ['ACTION_LEFT', 'NO_ACTION', 'ACTION_RIGHT']
+            - Param:
+                - actions: a list that contains three boolean value.
+            - Returns:
+                - reward: (int), terminal: (bool), self.score: (int)
+                - reward:
+                    - Doodler dies: -1
+                    - Doodler jumps on platforms: 2
+                - terminal:
+                    - Agent lives: True
+                    - Agent Dies: False
+                - score:
+                    - returns the current score of doodler in the game
+
+        """
+        last_cameray = self.cameray
+        terminal = False
+        reward = 0
+
+        if self.playery - self.cameray > 700:
+                self.gameReboot()
+                terminal = True
+                reward = -1
+                print("terminated")
+
+        self.screen.fill((255,255,255))
+        self.drawGrid()
+        if_score_add = self.drawPlatforms()
+        self.updatePlayerByAction(actions)
+        self.updatePlatforms()
+
+        if if_score_add:
+            reward = 2
+            self.timer = time.time()
+        else:
+            # check if doodler is on the same place for past 10 sec
+            # if so reboot()
+            if last_cameray == self.cameray:
+                if self.timer == None:
+                    self.timer = time.time()
+
+                elif self.timer != None:
+                    now_time = time.time()
+                    if (now_time - self.timer) > 10:
+                        self.gameReboot()
+
+                        terminal = True
+                        reward = -1
+                        print("terminated")
+                        self.timer = time.time()
+
+        pygame.display.update()
+        self.FPSCLOCK.tick(self.FPS)
+        return reward, terminal, self.score
+
+    def gameReboot(self):
+        """
+            - No Params & No Returns
+            - resets all elements of the game
+            - to be called when agent dies
+        """
+        self.cameray = 0
+        self.score = 0
+        self.die = 0
+        self.springs = []
+        self.monsters =[]
+        self.platforms = [[400, 500, 0, 0]]
+        self.generatePlatforms()
+        self.playerx = 400
+        self.playery = 400
+
     def run(self):
         clock = pygame.time.Clock()
         self.generatePlatforms()
@@ -180,21 +327,14 @@ class DoodleJump:
                 if event.type == QUIT:
                     sys.exit()
             if self.die==1 or (self.playery - self.cameray > 900):
-                self.cameray = 0
-                self.score = 0
-                self.die = 0
-                self.springs = []
-                self.monsters =[]
-                self.platforms = [[400, 500, 0, 0]]
-                self.generatePlatforms()
-                self.playerx = 400
-                self.playery = 400
+                self.gameReboot()
+
             self.drawGrid()
             self.drawPlatforms()
             self.updatePlayer()
             self.updatePlatforms()
             self.screen.blit(self.font.render(str(self.score), -1, (0, 0, 0)), (25, 25))
-            pygame.display.flip() 
+            pygame.display.flip()
 
 
 DoodleJump().run()
