9e53f04937b958794f267d73170810db98c58028
riya-kothari <riyakothari2356@gmail.com>
2021-02-15 20:19:48 -0500

    adding model and agent

---
+++ game/doodlejump.py
@@ -0,0 +1,355 @@
+import pygame
+from pygame.locals import *
+import sys
+import random
+import time
+
+path = './game/'
+class DoodleJump:
+    def __init__(self, FPS=30000):
+        self.screen = pygame.display.set_mode((800, 800))
+        self.green = pygame.image.load(path+"assets/green.png").convert_alpha()
+        pygame.font.init()
+        self.score = 0
+        self.font = pygame.font.SysFont("Arial", 25)
+        self.blue = pygame.image.load(path+"assets/blue.png").convert_alpha()
+        self.red = pygame.image.load(path+"assets/red.png").convert_alpha()
+        self.red_1 = pygame.image.load(path+"assets/red_1.png").convert_alpha()
+        self.playerRight = pygame.image.load(path+"assets/right.png").convert_alpha()
+        self.playerRight_1 = pygame.image.load(path+"assets/right_1.png").convert_alpha()
+        self.playerLeft = pygame.image.load(path+"assets/left.png").convert_alpha()
+        self.playerLeft_1 = pygame.image.load(path+"assets/left_1.png").convert_alpha()
+        self.spring = pygame.image.load(path+"assets/spring.png").convert_alpha()
+        self.spring_1 = pygame.image.load(path+"assets/spring_1.png").convert_alpha()
+        self.monster =pygame.image.load(path+"assets/monster1.png").convert_alpha()
+        self.direction = 0
+        self.playerx = 400
+        self.playery = 400
+        self.platforms = [[400, 500, 0, 0]]
+        self.springs = []
+        self.monsters =[]
+        self.cameray = 0
+        self.jump = 0
+        self.gravity = 0
+        self.xmovement = 0
+        self.die= 0
+        self.inter_platform_distance = 65
+        self.FPSCLOCK = clock = pygame.time.Clock()
+        self.FPS = FPS
+        self.timer = None
+        self.clock = pygame.time.Clock()
+        self.generatePlatforms()
+
+    def updatePlayer(self):
+        if self.die==1:
+            return
+        if not self.jump:
+            self.playery += self.gravity
+            self.gravity += 1
+        elif self.jump:
+            self.playery -= self.jump
+            self.jump -= 1
+        key = pygame.key.get_pressed()
+        if key[K_RIGHT]:
+            if self.xmovement < 10:
+                self.xmovement += 1
+            self.direction = 0
+
+        elif key[K_LEFT]:
+            if self.xmovement > -10:
+                self.xmovement -= 1
+            self.direction = 1
+        else:
+            if self.xmovement > 0:
+                self.xmovement -= 1
+            elif self.xmovement < 0:
+                self.xmovement += 1
+        if self.playerx > 850:
+            self.playerx = -50
+        elif self.playerx < -50:
+            self.playerx = 850
+        self.playerx += self.xmovement
+        if self.playery - self.cameray <= 200:
+            self.cameray -= 10
+        if not self.direction:
+            if self.jump:
+                self.screen.blit(self.playerRight_1, (self.playerx, self.playery - self.cameray))
+            else:
+                self.screen.blit(self.playerRight, (self.playerx, self.playery - self.cameray))
+        else:
+            if self.jump:
+                self.screen.blit(self.playerLeft_1, (self.playerx, self.playery - self.cameray))
+            else:
+                self.screen.blit(self.playerLeft, (self.playerx, self.playery - self.cameray))
+
+    def updatePlatforms(self):
+        for p in self.platforms:
+            rect = pygame.Rect(p[0], p[1], self.green.get_width() - 10, self.green.get_height())
+            player = pygame.Rect(self.playerx, self.playery, self.playerRight.get_width() - 10, self.playerRight.get_height())
+
+            if rect.colliderect(player) and self.gravity and self.playery < (p[1] - self.cameray):
+                if p[2] != 2:
+                    self.jump = 15
+                    self.gravity = 0
+                else:
+                    if p[-1] != 1:
+                        self.jump = 15 # jump even when you hit red broken platform
+                        p[-1] = 1
+                    else:
+                        self.jump = 0
+
+
+            # moving blue platform left and right
+            if p[2] == 1:
+                if p[-1] == 1:
+                    p[0] += 5
+                    if p[0] > 750:
+                        p[-1] = 0
+                else:
+                    p[0] -= 5
+                    if p[0] <= 0:
+                        p[-1] = 1
+
+    def drawPlatforms(self):
+        score_increment = False
+        for p in self.platforms:
+            # print("platform, ",(self.platforms))
+            check = self.platforms[0][1] - self.cameray
+            if check > 800:
+                platform = random.randint(0, 1000)
+                if platform < 800:
+                    platform = 0
+                elif platform < 900:
+                    platform = 1
+                else:
+                    platform = 2
+
+                self.platforms.append([random.randint(0, 700), self.platforms[-1][1] - self.inter_platform_distance, platform, 0])
+                coords = self.platforms[-1]
+                check = random.randint(0, 1000)
+                if check > 900 and platform == 0:
+                    self.springs.append([coords[0], coords[1] - 25, 0])
+
+                elif check>860 and platform == 0:
+                    self.monsters.append([coords[0], coords[1]- 50, 0])
+
+                print("popping ",self.platforms.pop(0))
+                self.score += 100
+                score_increment = True
+
+            if p[2] == 0:
+                self.screen.blit(self.green, (p[0], p[1] - self.cameray))
+            elif p[2] == 1:
+                self.screen.blit(self.blue, (p[0], p[1] - self.cameray))
+            elif p[2] == 2:
+                if not p[3]:
+                    self.screen.blit(self.red, (p[0], p[1] - self.cameray))
+                else:
+                    self.screen.blit(self.red_1, (p[0], p[1] - self.cameray))
+
+        for spring in self.springs:
+            if spring[-1]:
+                self.screen.blit(self.spring_1, (spring[0], spring[1] - self.cameray))
+            else:
+                self.screen.blit(self.spring, (spring[0], spring[1] - self.cameray))
+            if pygame.Rect(spring[0], spring[1], self.spring.get_width(), self.spring.get_height()).colliderect(pygame.Rect(self.playerx, self.playery, self.playerRight.get_width(), self.playerRight.get_height())):
+                self.jump = 25
+                self.cameray -= 25
+        for monster in self.monsters:
+            self.screen.blit(self.monster, (monster[0], monster[1] -self.cameray))
+            if pygame.Rect(monster[0], monster[1], self.monster.get_width(), self.monster.get_height()).colliderect(pygame.Rect(self.playerx, self.playery, self.playerRight.get_width(), self.playerRight.get_height())):
+                self.die=1
+
+        return score_increment
+
+    def generatePlatforms(self):
+        on = 800
+        while on > -100:
+            x = random.randint(0,700)
+            platform = random.randint(0, 1000)
+            if platform < 800:
+                platform = 0
+            elif platform < 900:
+                platform = 1
+            else:
+                platform = 2
+            self.platforms.append([x, on, platform, 0])
+            on -= self.inter_platform_distance
+
+    def drawGrid(self):
+        for x in range(80):
+            pygame.draw.line(self.screen, (222,222,222), (x * 12, 0), (x * 12, 800))
+            pygame.draw.line(self.screen, (222,222,222), (0, x * 12), (800, x * 12))
+
+    def getCurrentFrame(self):
+        """
+            - No Param function
+            - Returns
+                - A screengrab of current pygame display
+            - To be used by agent script to get current state
+        """
+        image_data = pygame.surfarray.array3d(pygame.display.get_surface())
+        return image_data
+
+    def updatePlayerByAction(self, actions):
+        """
+            - actions = ['ACTION_LEFT', 'NO_ACTION', 'ACTION_RIGHT']
+            - Param:
+                - actions: a list that contains three boolean value.
+            - To be used by playStep function in game script.
+        """
+        if not self.jump:
+            self.playery += self.gravity
+            self.gravity += 1
+        elif self.jump:
+            self.playery -= self.jump
+            self.jump -= 1
+
+        if actions[2]:
+            if self.xmovement < 10:
+                self.xmovement += 1
+            self.direction = 0
+
+        elif actions[0]:
+            if self.xmovement > -10:
+                self.xmovement -= 1
+            self.direction = 1
+
+        else: #action[1] is true
+            if self.xmovement > 0:
+                self.xmovement -= 1
+            elif self.xmovement < 0:
+                self.xmovement += 1
+
+        if self.playerx > 850:
+            self.playerx = -50
+        elif self.playerx < -50:
+            self.playerx = 850
+
+        # if self.playerx > 750:
+        #     #self.playerx = -50
+        #     self.playerx = 750
+        # elif self.playerx < 0:
+        #     #self.playerx = 850
+        #     self.playerx = 0
+
+        self.playerx += self.xmovement
+        if self.playery - self.cameray <= 200:
+            self.cameray -= 10
+        if not self.direction:
+            if self.jump:
+                self.screen.blit(self.playerRight_1, (self.playerx, self.playery - self.cameray))
+            else:
+                self.screen.blit(self.playerRight, (self.playerx, self.playery - self.cameray))
+        else:
+            if self.jump:
+                self.screen.blit(self.playerLeft_1, (self.playerx, self.playery - self.cameray))
+            else:
+                self.screen.blit(self.playerLeft, (self.playerx, self.playery - self.cameray))
+
+    def playStep(self, actions):
+        # print("playstep")
+        """
+            - actions = ['ACTION_LEFT', 'NO_ACTION', 'ACTION_RIGHT']
+            - Param:
+                - actions: a list that contains three boolean value.
+            - Returns:
+                - reward: (int), terminal: (bool), self.score: (int)
+                - reward:
+                    - Doodler dies: -1
+                    - Doodler jumps on platforms: 2
+                - terminal:
+                    - Agent lives: True
+                    - Agent Dies: False
+                - score:
+                    - returns the current score of doodler in the game
+        """
+        # self.generatePlatforms()
+        last_cameray = self.cameray
+        terminal = False
+        reward = 0
+
+        self.screen.fill((255,255,255))
+        self.clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+        if self.die==1 or (self.playery - self.cameray > 900):
+            self.gameReboot()
+            terminal = True
+            reward = -1
+            print("terminated")
+        
+        # if self.playery - self.cameray > 700:
+        #         self.gameReboot()
+                # terminal = True
+                # reward = -1
+                # print("terminated")
+
+        self.screen.fill((255,255,255))
+        self.drawGrid()
+        if_score_add = self.drawPlatforms()
+        self.updatePlayerByAction(actions)
+        self.updatePlatforms()
+
+        if if_score_add:
+            reward = 2
+            self.timer = time.time()
+        else:
+            # check if doodler is on the same place for past 10 sec
+            # if so reboot()
+            if last_cameray == self.cameray:
+                if self.timer == None:
+                    self.timer = time.time()
+
+                elif self.timer != None:
+                    now_time = time.time()
+                    if (now_time - self.timer) > 10:
+                        self.gameReboot()
+
+                        terminal = True
+                        reward = -1
+                        print("terminated")
+                        self.timer = time.time()
+
+        pygame.display.update()
+        self.FPSCLOCK.tick(self.FPS)
+        return reward, terminal, self.score
+
+    def gameReboot(self):
+        """
+            - No Params & No Returns
+            - resets all elements of the game
+            - to be called when agent dies
+        """
+        self.cameray = 0
+        self.score = 0
+        self.die = 0
+        self.springs = []
+        self.monsters =[]
+        self.platforms = [[400, 500, 0, 0]]
+        self.generatePlatforms()
+        self.playerx = 400
+        self.playery = 400
+
+    def run(self):
+        clock = pygame.time.Clock()
+        self.generatePlatforms()
+        while True:
+            self.screen.fill((255,255,255))
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    sys.exit()
+            if self.die==1 or (self.playery - self.cameray > 900):
+                self.gameReboot()
+
+            self.drawGrid()
+            self.drawPlatforms()
+            self.updatePlayer()
+            self.updatePlatforms()
+            self.screen.blit(self.font.render(str(self.score), -1, (0, 0, 0)), (25, 25))
+            pygame.display.flip()
+
+
+# DoodleJump().run()
---
+++ model.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Deep_QNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(4, 32, 8, 4, bias=True, padding=2)
+        # self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2, bias=True, padding=1)
+        # self.relu2 = nn.ReLU(inplace=True)
+        self.maxpool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1, bias=True, padding=1)
+        # self.relu3 = nn.ReLU(inplace=True)
+        self.maxpool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        # self.fc4 = nn.ReLU(inplace=True)
+        self.fc5 = nn.Linear(256, 3)
+        
+    def forward(self, x):
+      
+        # might have to reshape the input. Check with Pratik on how input will be then modify
+        # print("forward shape", x.shape)
+        x = x.view(-1,4,80,80)
+        x_torch = torch.Tensor(x)
+        # print(x_torch.shape)        
+        conv1_res = F.relu(self.conv1(x_torch))
+        maxpool1_res = self.maxpool1(conv1_res)
+
+        conv2_res = F.relu(self.conv2(maxpool1_res))
+        maxpool2_res = self.maxpool2(conv2_res)
+
+        conv3_res = F.relu(self.conv3(maxpool2_res))
+        maxpool3_res = self.maxpool3(conv3_res)
+
+        flattened_res = torch.reshape(maxpool3_res, (-1, 256))
+
+        # fc4_res = self.fc4(flattened_res)
+        fc5_res = self.fc5(F.relu(flattened_res))
+
+        # print(fc5_res.shape)
+        
+        return fc5_res
+
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        super(QTrainer, self). __init__()
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        # print("OKAY_training")
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
+        # print("state shape", state.shape)
+        if state.shape[0] == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+    
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
