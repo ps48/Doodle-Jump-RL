c4ff0d11f78a03965cc7bea4c2029fc605e92be7
AmulyaKatti <amulyap2@gmail.com>
2021-02-19 15:55:45 -0800

    DRQN code changes

---
+++ agent_drqn.py
@@ -0,0 +1,124 @@
+import torch
+import random
+import numpy as np
+import cv2
+from collections import deque
+import torch
+from game.doodlejump import DoodleJump
+from drqn.model_drqn import Deep_QNet, QTrainer
+from helper import plot
+
+# These 2 lines of code are for the code to run on mac. Facing some issues due to duplicate openMP libraries. Ignore these.
+'''
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+'''
+
+MAX_MEMORY = 10000
+IMAGE_H = 80
+IMAGE_W = 80
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0
+        self.gamma = 0.9
+        self.memory = deque(maxlen = MAX_MEMORY)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model = Deep_QNet() #input_size = [1,4,80,80], output_size = 80)
+        self.lr = LR
+        self.trainer = QTrainer(model = self.model, lr=self.lr, gamma=self.gamma, device=self.device)
+        self.ctr = 1
+
+
+    def get_state(self, game):
+        state = game.getCurrentFrame()
+        img = cv2.cvtColor(cv2.resize(state, (IMAGE_W, IMAGE_H)), cv2.COLOR_BGR2GRAY)
+        M = cv2.getRotationMatrix2D((IMAGE_W / 2, IMAGE_H / 2), 270, 1.0)
+        img = cv2.warpAffine(img, M, (IMAGE_H, IMAGE_W))
+        # NOTE: Uncomment to store images
+        # cv2.imwrite("image_dump/"+str(self.ctr)+".jpg", img)
+        # self.ctr+=1
+        state = np.expand_dims(img, axis=0)
+        return state
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
+        else:
+            mini_sample = self.memory
+
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        # random moves: tradeoff exploration / exploitation
+        self.epsilon = 80 - self.n_games
+        final_move = [0,0,0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float).to(self.device)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = DoodleJump()
+    print("Now playing")
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.playStep(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, [done])
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.gameReboot()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.model.save()
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
+train()
---
+++ drqn/model_drqn.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import os
+
+class Deep_QNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = (Variable(torch.zeros(1, 1, 256).float()), Variable(torch.zeros(1, 1, 256).float()))
+        self.conv1 = nn.Conv2d(1, 32, 8, 4, bias=True, padding=2)
+        self.maxpool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.conv2 = nn.Conv2d(32, 64, 4, 2, bias=True, padding=1)
+        self.maxpool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.conv3 = nn.Conv2d(64, 64, 3, 1, bias=True, padding=1)
+        self.maxpool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.rnn = nn.LSTM(256, 256, 1)
+        self.fc1 = nn.Linear(256, 128)
+        self.fc2 = nn.Linear(128, 3)
+
+    def forward(self, x):
+        x = x.view(-1,1,80,80)
+        conv1_res = F.relu(self.conv1(x))
+        maxpool1_res = self.maxpool1(conv1_res)
+        conv2_res = F.relu(self.conv2(maxpool1_res))
+        maxpool2_res = self.maxpool2(conv2_res)
+        conv3_res = F.relu(self.conv3(maxpool2_res))
+        maxpool3_res = self.maxpool3(conv3_res)
+        flattened_res = torch.reshape(maxpool3_res, (-1, 256))
+        #flattened_res = maxpool3_res.view(maxpool3_res.size(0), -1)
+        flattened_res = flattened_res.unsqueeze(1)
+        rnn_res, new_hidden = self.rnn(flattened_res, self.hidden)
+        fc1_res = self.fc1(rnn_res)
+        fc2_res = self.fc2(fc1_res)
+        fc2_res = fc2_res.squeeze(1)
+        return fc2_res
+
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, lr, gamma, device):
+        super(QTrainer, self). __init__()
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+        self.device = device
+        self.model.to(self.device)
+
+    def train_step(self, state, action, reward, next_state, done):
+        #print(self.model)
+        state = torch.tensor(state, dtype=torch.float).to(self.device)
+        next_state = torch.tensor(next_state, dtype=torch.float).to(self.device)
+        action = torch.tensor(action, dtype=torch.float).to(self.device)
+        reward = torch.tensor(reward, dtype=torch.float).to(self.device)
+        # (n, x)
+        if state.shape[0] == 1:
+            # (1, x)
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            target[idx][torch.argmax(action[idx]).item()] = Q_new
+
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
